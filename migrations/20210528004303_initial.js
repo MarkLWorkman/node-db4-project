323941684474a82fb93354c1fc58fdf5f318418a
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-27 20:57:54 -0400

    migrations and seeds done

---
+++ migrations/20210528004303_initial.js
@@ -0,0 +1,44 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 200).notNullable().unique();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_name", 200).notNullable().unique();
+      tbl.string("ingredient_unit", 50);
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments("step_id");
+      tbl.string("step_text", 200).notNullable();
+      tbl.string("step_number").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("step_ingredients", (tbl) => {
+      tbl.increments("step_ingredient_id");
+      tbl.float("quantity").notNullable();
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("step_ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
+};
